abb57bd9346c981f0a05b731ad2a4664584388fa
Vadim Dovgiy <vadim.dovgiy@mail.ru>
2023-10-29 19:08:46 +0500

    Update Main

--- src/main/java/org/bot/Main.java
+++ src/main/java/org/bot/Main.java
@@ -1,7 +1,29 @@
 package org.bot;
 
+import org.bot.bots.TelegramJokeBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+/**
+ * Класс, включающий ботов
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main main = new Main();
+        main.registerBotTg();
+    }
+
+    /**
+     * Подключение бота тг
+     */
+    void registerBotTg(){
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new TelegramJokeBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
