f040440e31c25bccb7bc8e5e41c4452748ec2fc6
Vadim Dovgiy <vadim.dovgiy@mail.ru>
2023-10-29 19:07:03 +0500

    Create class TelegramJokeBot

---
+++ src/main/java/org/bot/bots/TelegramJokeBot.java
@@ -0,0 +1,72 @@
+package org.bot.bots;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Класс телеграм бота
+ */
+public class TelegramJokeBot extends TelegramLongPollingBot {
+
+
+    /**
+     * Токен телеграм бота
+     */
+    private final String TOKEN = System.getenv("TG_TOKEN");
+    /**
+     * Имя бота
+     */
+    private final String BOT_NAME = System.getenv("TG_BOT_NAME");
+
+    /**
+     *Метод, который получает сообщение от пользователя и отправляет ему новое в ответ
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        // TODO
+        try {
+            if (update.hasMessage() && update.getMessage().hasText()){
+                Message textInMessage = update.getMessage();
+                String chatId = textInMessage.getChatId().toString();
+                SendMessage sendMessage = new SendMessage();
+
+                String response = "Ваше сообщение: " + textInMessage.getText();
+
+                sendMessage.setChatId(chatId);
+                sendMessage.setText(response);
+
+                execute(sendMessage);
+            }
+        }
+        catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Получить имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    /**
+     * Получить токен бота
+     */
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+
+    /**
+     * Парсер сообщений, отправляемых пользователем
+     */
+    private String parseMessage(String message){
+        // TODO
+        return null;
+    }
+}
