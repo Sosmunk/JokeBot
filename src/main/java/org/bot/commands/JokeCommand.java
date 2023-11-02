1505a008b3046b81c5a003fb671299db461f93c7
Andrei Mesilov <dronoldino@gmail.com>
2023-11-02 21:02:31 +0500

    JokeService interaction

--- src/main/java/org/bot/commands/JokeCommand.java
+++ src/main/java/org/bot/commands/JokeCommand.java
@@ -1,9 +1,18 @@
 package org.bot.commands;
 
+import org.bot.Joke;
+import org.bot.dao.JokeService;
+import org.bot.dao.JokeServiceImpl;
+
 /**
  * Команда /joke
  */
+
 public class JokeCommand implements BotCommand {
+    JokeService jokeService;
+    public JokeCommand() {
+        jokeService = JokeServiceImpl.getInstance();
+    }
     /**
      * Получить случайную шутку (/joke)
      * @param args аргументы команды
@@ -11,6 +20,8 @@ public class JokeCommand implements BotCommand {
      */
     @Override
     public String execute(String args) {
+        // TODO: работа с сервисом
+        Joke joke = jokeService.getRandomJoke();
         return null;
     }
 }
