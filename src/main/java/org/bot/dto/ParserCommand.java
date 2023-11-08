678c0d7cb6847a7f661a42db194cc6332a535973
Vadim Dovgiy <vadim.dovgiy@mail.ru>
2023-11-08 20:00:34 +0500

    Add parserCommand

---
+++ src/main/java/org/bot/dto/ParserCommand.java
@@ -0,0 +1,17 @@
+package org.bot.dto;
+
+/**
+ * Класс парсера команд, вводимых пользователем
+ */
+public class ParserCommand {
+    /**
+     * Возвращает отформатированное сообщение в виде CommandData
+     * @param message сообщение пользователя
+     * @return CommandData сообщение
+     */
+    public CommandData parseMessage(String message){
+        String[] messageParams = message.split(" ",2);
+        CommandData commandData = new CommandData(messageParams[0],(messageParams.length > 1) ? messageParams[1] : null);
+        return commandData;
+    }
+}
