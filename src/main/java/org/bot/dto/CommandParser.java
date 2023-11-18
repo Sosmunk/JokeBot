8b40e8097128c4b3636c0502a671a16d6e003f6f
Vadim Dovgiy <vadim.dovgiy@mail.ru>
2023-11-18 21:52:03 +0500

    Editing validation in "parseMessage"

--- src/main/java/org/bot/dto/CommandParser.java
+++ src/main/java/org/bot/dto/CommandParser.java
@@ -10,7 +10,7 @@ public class CommandParser {
      * @return CommandData сообщение
      */
     public CommandData parseMessage(String message){
-        if (message == null || message.isEmpty()) return new CommandData(null,null);
+        if (message == null || message.isEmpty() || message.isBlank()) return new CommandData(null,null);
         String[] messageParams = message.split(" ",2);
         return new CommandData(messageParams[0],(messageParams.length > 1) ? messageParams[1] : null);
     }
