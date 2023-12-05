bad5604caadd45a708a10312f638c91e515db76e
Andrei Mesilov <dronoldino@gmail.com>
2023-12-05 23:57:09 +0500

    javadoc

--- src/test/java/org/bot/FakeService.java
+++ src/test/java/org/bot/FakeService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * Моковый сервис для тестирования функционала
+ * Моковый сервис для тестирования функционала CommandProcessor
  */
 public class FakeService implements JokeService {
     /**
