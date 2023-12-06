bee750457f6d8260cb60068d971b8f48cf583f76
Vadim Dovgiy <vadim.dovgiy@mail.ru>
2023-12-07 01:05:16 +0500

    CommandData changed to a record

--- src/main/java/org/bot/dto/CommandData.java
+++ src/main/java/org/bot/dto/CommandData.java
@@ -1,44 +1,11 @@
 package org.bot.dto;
 
-import java.util.Objects;
-
 /**
  * Хранит в себе данные о командах, поступающие от пользователя
+ *
+ * @param command Команда (например /getJoke)
+ * @param args    Аргументы (например: id="12")
  */
-public class CommandData {
-    public CommandData(String command, String args) {
-        this.command = command;
-        this.args = args;
-    }
+public record CommandData(String command, String args) {
 
-    /**
-     * Команда (например /getJoke)
-     */
-    private final String command;
-
-    /**
-     * Аргументы (например: id="12")
-     */
-    private final String args;
-
-    public String getCommand() {
-        return command;
-    }
-
-    public String getArgs() {
-        return args;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CommandData that = (CommandData) o;
-        return Objects.equals(command, that.command) && Objects.equals(args, that.args);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(command, args);
-    }
 }
