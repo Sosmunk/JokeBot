78b9d8d2d0e2f400abea8a50d3ace9da3e38541c
Andrei Mesilov <dronoldino@gmail.com>
2023-12-08 10:38:55 +0500

    remove dataloader from main

--- src/main/java/org/bot/Main.java
+++ src/main/java/org/bot/Main.java
@@ -6,7 +6,6 @@ import org.bot.bots.VkJokeBot;
 import org.bot.commands.CommandProcessor;
 import org.bot.dao.JokeService;
 import org.bot.dao.JokeServiceImpl;
-import org.bot.utils.DataLoader;
 
 /**
  * Класс, для запуска программы
@@ -14,9 +13,7 @@ import org.bot.utils.DataLoader;
 
 public class Main {
     public static void main(String[] args) {
-        DataLoader dataLoader = new DataLoader();
         JokeService jokeService = new JokeServiceImpl();
-        dataLoader.populate(jokeService);
         CommandProcessor commandProcessor = new CommandProcessor(jokeService);
         TelegramJokeBot telegramJokeBot = new TelegramJokeBot(commandProcessor);
         VkJokeBot vkJokeBot = new VkJokeBot(commandProcessor);
