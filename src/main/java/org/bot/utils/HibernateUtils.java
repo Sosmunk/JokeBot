53990ebf09fe29a4997c7d9754c22107580fe497
Andrei Mesilov <dronoldino@gmail.com>
2023-12-10 17:06:03 +0500

    javadoc

--- src/main/java/org/bot/utils/HibernateUtils.java
+++ src/main/java/org/bot/utils/HibernateUtils.java
@@ -4,7 +4,16 @@ import org.bot.Joke;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+/**
+ * Утилитарный класс для Hibernate
+ */
 public class HibernateUtils {
+
+    /**
+     * Создать SessionFactory
+     *
+     * @return SessionFactory
+     */
     public SessionFactory createSessionFactory() {
         Configuration configuration = new Configuration();
         configuration.setProperty(
