b431cbd35c9b80b07193ca84b038a596d6efcdcc
Andrei Mesilov <dronoldino@gmail.com>
2023-12-14 22:52:24 +0500

    dao import

--- src/main/java/org/bot/Main.java
+++ src/main/java/org/bot/Main.java
@@ -3,9 +3,7 @@ package org.bot;
 import org.bot.bots.TelegramJokeBot;
 import org.bot.bots.VkJokeBot;
 import org.bot.commands.CommandProcessor;
-import org.bot.dao.JokeDAO;
-import org.bot.dao.JokeService;
-import org.bot.dao.JokeServiceImpl;
+import org.bot.dao.*;
 import org.bot.utils.HibernateUtils;
 import org.hibernate.SessionFactory;
 
