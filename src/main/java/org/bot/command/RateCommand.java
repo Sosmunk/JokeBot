6c2e1bb7e763550170e8426143dc0c77afeeb922
Vadim Dovgiy <vadim.dovgiy@mail.ru>
2023-12-22 01:23:23 +0500

    Replace const

--- src/main/java/org/bot/command/RateCommand.java
+++ src/main/java/org/bot/command/RateCommand.java
@@ -8,42 +8,41 @@ import org.bot.service.RatingService;
  */
 public class RateCommand implements BotCommand {
 
-    private final RatingService ratingService;
+	private final RatingService ratingService;
+	private static final String INVALID_ARGUMENT_COUNT = "Неверное количество аргументов";
 
-    public RateCommand(RatingService ratingService) {
-        this.ratingService = ratingService;
-    }
+	public RateCommand(RatingService ratingService) {
+		this.ratingService = ratingService;
+	}
 
-    private static final String INVALID_ARGUMENT_COUNT = "Неверное количество аргументов";
+	@Override
+	public String execute(String args, Long chatId) {
+		if (args == null) {
+			return INVALID_ARGUMENT_COUNT;
+		}
+		String[] data = args.split(" ");
+		if (data.length != 2) {
+			return INVALID_ARGUMENT_COUNT;
+		}
 
-    @Override
-    public String execute(String args, Long chatId) {
-        if (args == null) {
-            return INVALID_ARGUMENT_COUNT;
-        }
-        String[] data = args.split(" ");
-        if (data.length != 2) {
-            return INVALID_ARGUMENT_COUNT;
-        }
-
-        Integer jokeId = Integer.parseInt(data[0]);
-        byte stars;
-        try {
-            stars = Byte.parseByte(data[1]);
-        } catch (NumberFormatException e) {
-            return "Количество звезд рейтинга должно содержать только цифры (1-5)";
-        }
+		Integer jokeId = Integer.parseInt(data[0]);
+		byte stars;
+		try {
+			stars = Byte.parseByte(data[1]);
+		} catch (NumberFormatException e) {
+			return "Количество звезд рейтинга должно содержать только цифры (1-5)";
+		}
 
 
-        if (stars > 5 || stars < 1) {
-            return "Неверное количество звезд рейтинга";
-        }
+		if (stars > 5 || stars < 1) {
+			return "Неверное количество звезд рейтинга";
+		}
 
-        boolean result = this.ratingService.rateJoke(jokeId, chatId, stars);
-        if (result) {
-            return "Анекдот оценен";
-        } else {
-            return "Анекдот не найден";
-        }
-    }
+		boolean result = this.ratingService.rateJoke(jokeId, chatId, stars);
+		if (result) {
+			return "Анекдот оценен";
+		} else {
+			return "Анекдот не найден";
+		}
+	}
 }
