2fcdf3e1c348ef99db314df0a4383d6b546428c5
Vadim Dovgiy <vadim.dovgiy@mail.ru>
2023-12-22 23:30:49 +0500

    Minor edits

--- src/main/java/org/bot/command/JokeCommand.java
+++ src/main/java/org/bot/command/JokeCommand.java
@@ -12,32 +12,31 @@ import java.util.Optional;
  * Отправить случайный анекдот
  */
 public class JokeCommand implements BotCommand {
-    private final JokeService jokeService;
-    private final RatingService ratingService;
+	private final JokeService jokeService;
+	private final RatingService ratingService;
 
-    public JokeCommand(JokeService jokeService, RatingService ratingService) {
-        this.jokeService = jokeService;
-        this.ratingService = ratingService;
-    }
+	public JokeCommand(JokeService jokeService, RatingService ratingService) {
+		this.jokeService = jokeService;
+		this.ratingService = ratingService;
+	}
 
-    @Override
-    public void execute(String args, Long chatId, Bot bot) {
-        Joke joke = jokeService.getRandomJoke();
-        if (joke == null) {
-            bot.sendMessage(chatId, "Анекдоты не найдены");
-            return;
-        }
+	@Override
+	public void execute(String args, Long chatId, Bot bot) {
+		Joke joke = jokeService.getRandomJoke();
+		if (joke == null) {
+			bot.sendMessage(chatId, "Анекдоты не найдены");
+			return;
+		}
 
-        jokeService.saveLastJoke(chatId, joke.getId());
+		jokeService.saveLastJoke(chatId, joke.getId());
 
-        Optional<Double> averageRating = ratingService.getAverageRatingForJoke(joke.getId());
+		Optional<Double> averageRating = ratingService.getAverageRatingForJoke(joke.getId());
 
-        String ratingString = averageRating.isPresent()
-                ? "\nРейтинг анекдота: " + averageRating.get()
-                : "";
+		String ratingString = averageRating.map(aDouble -> "\nРейтинг анекдота: " + aDouble)
+				.orElse("");
 
-        bot.sendMessageWithRateKeyboard(chatId, "Анекдот №" + joke.getId() +
-                "\n" + joke.getText() + ratingString);
+		bot.sendMessageWithRateKeyboard(chatId, "Анекдот №" + joke.getId() +
+				"\n" + joke.getText() + ratingString);
 
-    }
+	}
 }
